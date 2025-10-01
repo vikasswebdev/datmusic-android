917b238b3760d0128172eb60cdd426419d42e27e
VIKAS PATEL <63722887+vikasswebdev@users.noreply.github.com>
2025-10-01 10:57:48 +0530

    Upgrade build tooling for Gradle 8.5 and Kotlin 1.9

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,7 +1,22 @@
-plugins {
-    `kotlin-dsl`
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
+    }
 }
 
+apply(plugin = "org.jetbrains.kotlin.jvm")
+
 repositories {
-    jcenter()
+    google()
+    mavenCentral()
+    gradlePluginPortal()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.22")
 }
